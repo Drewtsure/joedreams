9ef0608298cc588cb31040d3ec6032acea9d5cca
Andrew Zakharchenko <drewtsure@gmail.com>
2015-12-09 02:55:43 +0300

    немного магии и начало стилей
    добавил csscomb и watch

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,8 @@
 module.exports = function(grunt){
 
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-csscomb');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.initConfig({
 
@@ -9,6 +11,22 @@ module.exports = function(grunt){
         files: {
           "css/style.css": ["scss/style.scss"]
         }
+      }
+    },
+    csscomb: {
+      dist: {
+        options: {
+          config: 'csscomb.json'
+        },
+        files: {
+          'css/style.css': ['css/style.css'],
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass,csscomb']
       }
     }
   });
